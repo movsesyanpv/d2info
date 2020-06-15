803252cfd4a85bbe6f3d610f63a8626009dc2b39
movsesyanpv <movsesyanpv1995@live.com>
2020-06-15 16:39:38 +0300

    fixed Sanic port

--- main.py
+++ main.py
@@ -262,7 +262,7 @@ class D2info:
         app.static('/static', './static')
         # app.url_for('static', filename='style.css', name='style')
         if self.args.production:
-            app.run(host='0.0.0.0', port='1423')
+            app.run(host='0.0.0.0', port=1423)
         else:
             app.run()
 
