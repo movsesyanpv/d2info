98c161121ae92bb6084c825b092e185a90ab5201
movsesyanpv <movsesyanpv1995@live.com>
2021-12-07 23:32:05 +0300

    Swapped some categories

--- d2data.py
+++ d2data.py
@@ -670,12 +670,12 @@ class D2data:
                 is_interesting = True
                 cat_number = 2
                 data_index = 0
-            elif item['displayCategoryIndex'] == 9 and item['itemHash'] not in [353932628, 3260482534, 3536420626,
+            elif item['displayCategoryIndex'] == 8 and item['itemHash'] not in [353932628, 3260482534, 3536420626,
                                                                                 3187955025, 2638689062]:
                 is_interesting = True
                 cat_number = 7
                 data_index = 1
-            elif item['displayCategoryIndex'] == 8 and item['itemHash'] not in [353932628, 3260482534, 3536420626,
+            elif item['displayCategoryIndex'] == 9 and item['itemHash'] not in [353932628, 3260482534, 3536420626,
                                                                                  3187955025, 2638689062]:
                 is_interesting = True
                 cat_number = 9
